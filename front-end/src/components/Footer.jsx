554cd92f2109ba89de5c0b25fd2651e67c7a1543
gabo0802 <gabriel.castejon0802@gmail.com>
2025-03-01 16:01:30 -0500

    Added basic header and footer

---
+++ front-end/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+
+export default function Footer() {
+    return(
+        <footer className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <h3>Footer</h3>
+                        <p>Footer content</p>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <h3>Footer</h3>
+                        <p>Footer content</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
---
+++ front-end/src/components/Header.jsx
@@ -0,0 +1,19 @@
+
+export default function Header() {
+    return(
+        <footer className="header">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <h3>Header</h3>
+                        <p>Header content</p>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <h3>Header</h3>
+                        <p>Header content</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
