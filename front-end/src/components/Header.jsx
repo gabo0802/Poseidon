bc72a419422a612ab2d88757a60bbe27e3eb3838
gabo0802 <gabriel.castejon0802@gmail.com>
2025-03-01 17:01:13 -0500

    Header changes

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -2,17 +2,15 @@
 export default function Header() {
     return(
         <header className="header">
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 col-md-6 text-center">
-                        <h3>Header</h3>
-                        <p>Header content</p>
-                    </div>
-                    <div className="col-12 col-md-6 text-center">
-                        <h3>Header</h3>
-                        <p>Header content</p>
-                    </div>
-                </div>
+            <div className="w-full h-[138px] relative">
+            <div className="w-full h-[138px] left-0 top-0 absolute bg-[#0825fb]/70 border border-black" />
+            <div className="w-[1007px] h-[38px] left-[173px] top-[57px] absolute">
+            <h3 className="left-[467px] top-[2px] absolute text-black text-2xl font-bold font-['Poppins']">Home</h3>
+            <h3 className="left-0 top-0 absolute text-black text-2xl font-bold font-['Poppins']">Presidon</h3>
+            <h3 className="left-[639px] top-[2px] absolute text-black text-2xl font-normal font-['Poppins']">Description</h3>
+            <h3 className="left-[872px] top-[2px] absolute text-black text-2xl font-normal font-['Poppins']">Flood Map</h3>
+            </div>
+            <img className="w-[63px] h-[63px] left-[94px] top-[31px] absolute" src="src/assets/presidon-logo.png"/>
             </div>
         </header>
     )
