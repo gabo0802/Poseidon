2d199ae3bafdd830db28f71e9b433482338ad9ff
Gael Garcia <gael.garcia@ufl.edu>
2025-03-01 18:45:21 -0500

    temp changes

---
+++ .history/front-end/src/App_20250301184257.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Map from "./components/Map";
+import LandingPage from "./components/LandingPage";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <LandingPage />
+      <Map />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
