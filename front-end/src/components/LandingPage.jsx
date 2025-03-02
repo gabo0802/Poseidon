e2b9a501c3aa581568d32a619a961c8a06710f2f
Gael Garcia <148827100+GG1627@users.noreply.github.com>
2025-03-02 11:48:37 -0500

    Update LandingPage Card

--- front-end/src/components/LandingPage.jsx
+++ front-end/src/components/LandingPage.jsx
@@ -119,10 +119,7 @@ function LandingPage() {
                       </h2>
                     </div>
                     <p className="m-10 text-xl">
-                      With 91% accuracy, our AI-powered model helps predict flash floods before they strike, 
-                      enabling proactive measures to safeguard communities. 
-                      By leveraging real-time data and historical trends, 
-                      we shift disaster response from reactive to preventative, protecting lives and infrastructure.
+                     In 2024, record-breaking floods in Florida forced over 1 million evacuations, submerged thousands of homes, and left 500,000 people homeless.
                     </p>
                   </div>
                 </motion.div>
