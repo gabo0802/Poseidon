44ebff1cc50eb6c58ba94df7e9a1b66997f0fcd9
gabo0802 <gabriel.castejon0802@gmail.com>
2025-03-01 19:51:32 -0500

    Updates to Landing Page

--- front-end/src/components/LandingPage.jsx
+++ front-end/src/components/LandingPage.jsx
@@ -3,13 +3,23 @@ import backgroundImage from '../../public/background-img.jpg'; // Correctly impo
 
 function LandingPage() {
     return (
+        <div>
         <div 
             className="box-border bg-opacity-90 bg-cover bg-center bg-no-repeat min-h-screen margin-0 padding-0"
             style={{ backgroundImage: `url(${backgroundImage})` }} 
         >
-            <div className="flex justify-center t-20 h-screen">
-            <div className="text-white text-4xl font-bold">
-                Welcome to Presidon
+            <div className="flex justify-center -translate-y-[8em] h-screen">
+            <div className="flex flex-col justify-center items-center">
+            <div className="text-8xl font-bold text-black">
+                Presidon
+            </div>
+            <div className="text-4xl font-bold text-black">
+                Florida's Flood Risk Management Platform
+            </div>
+            <br></br>
+            <br></br>
+            <div>test div</div>
+            </div>
             </div>
             </div>
         </div>
