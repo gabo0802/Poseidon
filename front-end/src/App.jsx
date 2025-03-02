131c68b3af8401510741c6fd44e8a97ad5ca6b9a
alejgonza04 <149022594+alejgonza04@users.noreply.github.com>
2025-03-02 09:35:28 -0500

    front end deployment

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import Map from "./components/Map";
 import LandingPage from "./components/LandingPage";
 
@@ -9,8 +9,6 @@ function App() {
       <Header />
       <LandingPage />
       <Map />
-      {/* Uncomment to test API <ApiCaller /> */}
-
       <Footer />
     </div>
   );
